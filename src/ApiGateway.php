50495793d9aaa1571e433a65d72ba5a1b65c93ba
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-09-23 09:54:31 +0200

    Added support for POST calls

--- src/ApiGateway.php
+++ src/ApiGateway.php
@@ -12,9 +12,18 @@ class ApiGateway
      */
     public function execute($path)
     {
-        return Zttp::withHeaders([
-            'Authorization' => 'Bearer '.config('harvest.api_key'),
-            'Harvest-Account-Id' => config('harvest.account_id'),
-        ])->get($path['url'] ?? $path);
+        if ($path['method'] === 'POST') {
+            return Zttp::withHeaders([
+                'Authorization' => 'Bearer '.config('harvest.api_key'),
+                'Harvest-Account-Id' => config('harvest.account_id'),
+            ])->post($path['url'], $path['body']);
+        } else if ($path['method'] === 'GET') {
+            return Zttp::withHeaders([
+                'Authorization' => 'Bearer '.config('harvest.api_key'),
+                'Harvest-Account-Id' => config('harvest.account_id'),
+            ])->get($path['url'] ?? $path);
+        } else {
+            throw new \Exception('HTTP verb ' . $path['method'] . ' is not supported.');
+        }
     }
 }
