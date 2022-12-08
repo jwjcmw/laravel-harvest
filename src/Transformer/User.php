1d670ff09ecef1a939e4aea52896696679465ceb
Peter Jaap Blaakmeer <peterjaap@blaakmeer.com>
2022-12-08 15:03:45 +0100

    Set defaults

--- src/Transformer/User.php
+++ src/Transformer/User.php
@@ -27,11 +27,11 @@ class User implements Transformer
         $user->timezone = $data['timezone'];
         $user->has_access_to_all_future_projects = $data['has_access_to_all_future_projects'];
         $user->is_contractor = $data['is_contractor'];
-        $user->is_admin = $data['is_admin'];
-        $user->is_project_manager = $data['is_project_manager'];
-        $user->can_see_rates = $data['can_see_rates'];
-        $user->can_create_projects = $data['can_create_projects'];
-        $user->is_active = $data['is_active'];
+        $user->is_admin = $data['is_admin'] ?? false;
+        $user->is_project_manager = $data['is_project_manager'] ?? false;
+        $user->can_see_rates = $data['can_see_rates'] ?? false;
+        $user->can_create_projects = $data['can_create_projects'] ?? false;
+        $user->is_active = $data['is_active'] ?? false;
         $user->weekly_capacity = $data['weekly_capacity'];
         $user->default_hourly_rate = $data['default_hourly_rate'];
         $user->cost_rate = $data['cost_rate'];
