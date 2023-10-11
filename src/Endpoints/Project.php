570cc8460ce76c6122654cb89f1a12714673abe3
Jim Wright <jim@wrightster.com>
2023-10-11 16:19:40 -0400

    Update Project.php

--- src/Endpoints/Project.php
+++ src/Endpoints/Project.php
@@ -20,13 +20,4 @@ class Project extends BaseEndpoint
     {
         $this->params += ['client_id' => $id];
     }
-
-    public function updatedSince(string|Carbon $dateTime): void
-    {
-        if (! $dateTime instanceof Carbon) {
-            $dateTime = Carbon::parse($dateTime);
-        }
-
-        $this->params += ['updated_since' => $dateTime->toIso8601ZuluString()];
-    }
 }
